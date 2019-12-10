ea9dcc61ac8b0c2b71d7e70c4c72054554162b49
Maciej Adamski <48189515+madamskip1@users.noreply.github.com>
2019-12-10 01:35:25 +0100

    Add files via upload

--- stupid_pattern/function.s
+++ stupid_pattern/function.s
@@ -7,41 +7,51 @@ function:
 
     ; rdi   string
 
-    mov r13, rdi
-    mov dl, [r13]
+
+    mov dl, [rdi]
+    cmp dl, 0
+    jz end
+    dec rdi
+
+loopA:
+inc rdi
+    mov dl, [rdi]
     cmp dl, 0
     jz end
 
-loopA:
-    mov dl, [r13]
-    cmp dl, 0
-    jz endstring
+first:
+    cmp dl, 90
+    jle second
 
-    cmp dl, 48
-    jl writeChar
-
-    cmp dl, 57
-    jg writeChar
-
-    test dl, 1
-    jnz writeChar
-
-    jmp next
-
-writeChar:
+    sub dl, 32
     mov [rdi], dl
+
+second:
+
+    inc rdi
+    mov dl, [rdi]
+
+    cmp dl, 97
+    jge third
+
+    add dl, 32
+    mov [rdi], dl
+
+third:
     inc rdi
 
+fourth: 
+    inc rdi
 
-next:
-    inc r13
+    mov dl, [rdi]
+
+    cmp dl, 97
+    jge loopA
+
+    add dl, 32
+    mov [rdi], dl
+
     jmp loopA
-
-
-
-
-endstring:
-    mov [rdi], byte 0
 
 end:
     mov rsp, rbp
